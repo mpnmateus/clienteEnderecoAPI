14ed08aa13d22319f6c34b08f55dbcb1bdc48b10
mateusnovo <mpnmateus@gmail.com>
2024-10-18 15:06:38 -0300

    init

---
+++ src/main/java/com/clienteEnderecoAPI/ClienteEnderecoApiApplication.java
@@ -0,0 +1,13 @@
+package com.clienteEnderecoAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClienteEnderecoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClienteEnderecoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clienteEnderecoAPI/ClienteEnderecoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clienteEnderecoAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteEnderecoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
