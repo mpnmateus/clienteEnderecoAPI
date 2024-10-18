99cdc2fffc999a3075faa7c2759ade5e27320742
mateusnovo <mpnmateus@gmail.com>
2024-10-18 19:38:46 -0300

    feat: classe que implementa concretamente a interface de service de cliente -> TO-DO

---
+++ src/main/java/com/clienteEnderecoAPI/service/impl/ClienteServiceImpl.java
@@ -0,0 +1,31 @@
+package com.clienteEnderecoAPI.service.impl;
+
+import com.clienteEnderecoAPI.model.Cliente;
+import com.clienteEnderecoAPI.service.ClienteService;
+
+public class ClienteServiceImpl implements ClienteService {
+    @Override
+    public Iterable<Cliente> buscarTodos() {
+        return null;
+    }
+
+    @Override
+    public Cliente buscarPorId(Long id) {
+        return null;
+    }
+
+    @Override
+    public void inserir(Cliente cliente) {
+
+    }
+
+    @Override
+    public void atualizar(Long id, Cliente cliente) {
+
+    }
+
+    @Override
+    public void deletar(Long id) {
+
+    }
+}
