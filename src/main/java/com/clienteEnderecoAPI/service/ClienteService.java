07329a5d48a834ecc9dc0283d7e5c9273b17568e
mateusnovo <mpnmateus@gmail.com>
2024-10-18 19:36:10 -0300

    feat: interface de service para cliente, aguardando implementacao concreta

---
+++ src/main/java/com/clienteEnderecoAPI/service/ClienteService.java
@@ -0,0 +1,12 @@
+package com.clienteEnderecoAPI.service;
+
+import com.clienteEnderecoAPI.model.Cliente;
+
+public interface ClienteService {
+
+    Iterable<Cliente> buscarTodos();
+    Cliente buscarPorId(Long id);
+    void inserir(Cliente cliente);
+    void atualizar(Long id, Cliente cliente);
+    void deletar(Long id);
+}
